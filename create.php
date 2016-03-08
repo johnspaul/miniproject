e93de2723ecf1df3af8b08d5fa52964cb05807de
johnspaul <johnspaul007@gmail.com>
2016-03-08 08:21:07 +0530

    product display ready with reswponsiveness

---
+++ create.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<meta charset="utf-8">
+<html>
+    <body>
+    <form enctype="multipart/form-data" method="post" action="upload.php">
+    <label for="gname">Gift name</label><input type="text" name="gname">
+  <input type="file" size="32" name="image_field" value="">
+  <input type="submit" name="Submit" value="upload">
+
+        </form>
+    </body>
+</html>
--- products.php
+++ products.php
@@ -3,13 +3,15 @@
 <html>
     <head>
     <link rel="stylesheet" href="css/products.css">
+        <link href='https://fonts.googleapis.com/css?family=Josefin+Slab' rel='stylesheet' type='text/css'>
     </head>
 <body>
+    <div id="head"><img id="logo" src="images/logo2.png"> <a href="#">IFT<span id="aa">A</span>GIFT</a></div>
     <?php
     
 $db=mysqli_connect('localhost','root','','giftagift')OR DIE('cannot connect');
     mysqli_select_db($db,'giftagift');
-    $sql="select * from gifts";
+    $sql="select * from products";
 $res=mysqli_query($db,$sql);
     $count=mysqli_num_rows($res);
     ?>
@@ -21,12 +23,16 @@ $res=mysqli_query($db,$sql);
         <li class="box">
     <a href="#">
         <img src="<?php echo $row['gimage']; ?>">
+       
              <h4> <?php echo $row['gname']; ?></h4>
             </a>
         </li>
     <?php
     }
         ?>
+        <script>
+    
+        </script>
     </ul>
     </body>
 </html>
